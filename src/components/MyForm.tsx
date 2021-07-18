d3b7a1f1e3f1b0754c5d6368eeab598e09ee7534
nguoianphu <nguoianphu@gmail.com>
2021-07-18 15:01:25 +0700

    onIonChange - settext and gettext

--- src/components/MyForm.tsx
+++ src/components/MyForm.tsx
@@ -8,21 +8,28 @@ interface ContainerProps {
 
 const MyForm: React.FC<ContainerProps> = ({ name }) => {
   const [text, setText] = useState<string>();
+
+  function myCoords (coords: string){
+    console.log(coords)
+    return coords;
+  }
+
   return (
     <IonList>
       <IonItemDivider>All text</IonItemDivider>
       <IonItem>
-        <IonTextarea clearOnEdit={true}  value={text} onIonChange={e => setText(e.detail.value!)}></IonTextarea>
+        <IonTextarea clearOnEdit={true} value={text} onIonChange={e => {setText(e.detail.value!);myCoords(e.detail.value!)}}></IonTextarea>
       </IonItem>
-
       <IonItemDivider>Coords</IonItemDivider>
       <IonItem>
-        <IonInput value={text} placeholder="Enter Input" onIonChange={e => setText(e.detail.value!)}></IonInput>
+        <IonInput value={text} placeholder="Enter Input" onIonChange={e => setText(myCoords(e.detail.value!))}></IonInput>
       </IonItem>
 
     </IonList>
   );
 };
+
+
 
 export default MyForm;
 
