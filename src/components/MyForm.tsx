e0461823657ae1a67f214461e0605508a1684ce2
nguoianphu <nguoianphu@gmail.com>
2021-07-18 17:44:58 +0700

    test regex

--- src/components/MyForm.tsx
+++ src/components/MyForm.tsx
@@ -9,16 +9,41 @@ interface ContainerProps {
 const MyForm: React.FC<ContainerProps> = ({ name }) => {
   const [text, setText] = useState<string>();
 
-  function myCoords (coords: string){
-    console.log(coords)
-    return coords;
+  function myCoords(coords: string) {
+    let regexpCoords = new RegExp('^[+ 0-9]{5}$');
+    // console.log(coords.match(regexpCoords));
+    // console.log(coords)
+    // 41.651727,-0.881560
+
+    var searchValue = coords;
+    var regex = /^[+ 0-9]$/;
+    // var regex = new RegExp('([-+]?)([\d]{1,2})(((\.)(\d+)(,)))(\s*)(([-+]?)([\d]{1,3})((\.)(\d+))?)')
+    var match = regex.exec(coords);
+    // console.log('search results', coords);
+    if (match) {
+      // var coordinates = [parseFloat(match[1]), parseFloat(match[2])];
+      console.log("MATCHED");
+      console.log(match[0]);
+      console.log(match[1]);
+      console.log(match[2]);
+
+      
+      return coords;
+
+    } else {
+
+      return coords;
+
+    }
+
+
   }
 
   return (
     <IonList>
       <IonItemDivider>All text</IonItemDivider>
       <IonItem>
-        <IonTextarea clearOnEdit={true} value={text} onIonChange={e => {setText(e.detail.value!);myCoords(e.detail.value!)}}></IonTextarea>
+        <IonTextarea clearOnEdit={true} value={text} onIonChange={e => { setText(e.detail.value!); myCoords(e.detail.value!) }}></IonTextarea>
       </IonItem>
       <IonItemDivider>Coords</IonItemDivider>
       <IonItem>
